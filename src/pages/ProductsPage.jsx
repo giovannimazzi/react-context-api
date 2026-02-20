29c0d61a8506a2a6385e9946259b5d95a40b712d
giovannimazzi <gioelor@libero.it>
2026-02-20 17:55:14 +0100

    feat: add filter in productPage which on the basis of the budgetMode value shows only the products with price <=30 (true) or all the products (false).

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,20 +1,25 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
+import { useBudget } from "../contexts/BudgetContext";
 
 const productsApiUrl = "https://fakestoreapi.com/products";
 
 export default function ProductsPage() {
   const [products, setProducts] = useState([]);
 
+  const { budgetMode } = useBudget();
+
   useEffect(() => {
     axios
       .get(productsApiUrl)
       .then((res) => {
-        setProducts(res.data);
+        budgetMode
+          ? setProducts(res.data.filter((p) => Number(p.price) <= 30))
+          : setProducts(res.data);
       })
       .catch((e) => alert("ERRORE richiesta API: \n\n" + e.message));
-  }, []);
+  }, [budgetMode]);
 
   return (
     <>
